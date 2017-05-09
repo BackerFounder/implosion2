db87f52f9c9f0710c3147a48b5246a3d0993a17c
bunnieabc <bunnieabc@gmail.com>
2017-05-09 17:30:38 +0800

    add modal effect

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -17,21 +17,13 @@ $(document).ready(function() {
 
 });
 
-//reveal效果
-window.sr = ScrollReveal();
-sr.reveal('.sec1 .reveal',300);
-sr.reveal('.sec2 .reveal',400);
-sr.reveal('.sec3 .reveal',400);
-sr.reveal('.sec4 .reveal',400);
-sr.reveal('.sec5 .reveal',340);
+// FANCY BOX
+$(".btn").click(function(){
+  $(".modal-bg").css("display","flex");
+})
+// close
+$(".close").click(function(){
+  $(".modal-bg").css("display","none");
 
-
-// 視差滾動
-$(window).load(function() {
-    var scene = document.getElementById('scene');
-    var parallax = new Parallax(scene);
-});
-
-
-
+})
 
