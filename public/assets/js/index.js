d9d1483c5c3a365a758b4cda6fb0634fd136c46e
bunnieabc <bunnieabc@gmail.com>
2017-05-10 11:27:31 +0800

    add video script and add character show up script

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,49 +1,37 @@
 $(document).ready(function() {
-  //loading
-  $(window).load(function() {
-    $("#loading").delay(600).fadeOut();
-  });
-  var progress = 0;
-  $("img").load(function() {
-    progress += 100 / $("img").length;
-    var percent = Math.round(progress) + "%";
-    $("#loading .txt").text(percent);
-    $("#loading .ld-bar").css("width", percent);
-  });
-  //loading END
-
-  $(".number-20").animateNumbers(20, true, 2800);
-  $(".number-100").animateNumbers(100, true, 3800);
-
+  $(".character").fadeIn(1000)
+  // init the character position
+  $('.popup')
+    .css('left', 0  +'px')
+    .css('top', 0 +'px')
 });
 
-// FANCY BOX
+// btn click and modal show up
 $(".btn").click(function(){
   $(".modal-bg").css("display","flex");
-  var src = $(".modal-content iframe").attr('src');
+  var src = $("#video").attr('src');
     $(".modal-content iframe").attr('src', src);
 })
-// close modal
+// close modal and video stop
 $(".close").click(function(){
   $(".modal-bg").css("display","none");
-
+  $('#video').attr('src', '');
 })
 // 3d move
 var moveForce = 30; // max popup movement in pixels
 var rotateForce = 20; // max popup rotation in deg
 
 $(document).mousemove(function(e) {
-    var docX = $(document).width();
-    var docY = $(document).height();
+  var docX = $(document).width();
+  var docY = $(document).height();
 
-    var moveX = (e.pageX - docX/2) / (docX/2) * -moveForce;
-    var moveY = (e.pageY - docY/2) / (docY/2) * -moveForce;
+  var moveX = (e.pageX - docX/2) / (docX/2) * -moveForce;
+  var moveY = (e.pageY - docY/2) / (docY/2) * -moveForce;
 
-    var rotateY = (e.pageX / docX * rotateForce*2) - rotateForce;
-    var rotateX = -((e.pageY / docY * rotateForce*2) - rotateForce);
-
-    $('.popup')
-        .css('left', moveX * 0.5  +'px')
-        .css('top', moveY * 0.5 +'px')
-        .css('transform', 'rotateX('+rotateX * 0.5+'deg) rotateY('+rotateY * 0.5+'deg)');
+  var rotateY = (e.pageX / docX * rotateForce*2) - rotateForce;
+  var rotateX = -((e.pageY / docY * rotateForce*2) - rotateForce);
+  console.log(moveX, moveY)
+  $('.popup')
+    .css('left', moveX * 0.5  +'px')
+    .css('top', moveY * 0.5 +'px')
 });
